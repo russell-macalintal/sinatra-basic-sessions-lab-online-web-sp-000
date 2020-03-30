1c46c8f07fc42e278f82fa44474afa32215075c9
Russell Macalintal <russell.macalintal@gmail.com>
2020-03-30 02:50:42 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
+  
+  get '/' do
+    erb :index
+  end
+
+  get '/checkout' do
+    @session = session
+    erb :checkout
+  end
 end
